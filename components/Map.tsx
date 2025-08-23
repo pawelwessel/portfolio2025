cb1b4e9871a90f694e529d00fea58c0ca078af62
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-08-23 16:28:12 +0200

    overflow fix

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import AOS from "aos";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
 type MapMarker = {
@@ -20,6 +20,8 @@ type MapProps = {
 };
 
 export default function Map({ markers }: MapProps) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   useEffect(() => {
     AOS.init({
       offset: 0,
@@ -27,35 +29,36 @@ export default function Map({ markers }: MapProps) {
   }, []);
   return (
     <div className="relative w-full h-full">
-      {/* Markers */}
-      {markers.map((marker) => {
-        const leftClass = marker.leftSm
-          ? `left-[${marker.left}] sm:left-[${marker.leftSm}]`
-          : `left-[${marker.left}]`;
-        const topClass = marker.topSm
-          ? `top-[${marker.top}] sm:top-[${marker.topSm}]`
-          : `top-[${marker.top}]`;
+      {/* Markers - only render after image is loaded */}
+      {imageLoaded &&
+        markers.map((marker) => {
+          const leftClass = marker.leftSm
+            ? `left-[${marker.left}] sm:left-[${marker.leftSm}]`
+            : `left-[${marker.left}]`;
+          const topClass = marker.topSm
+            ? `top-[${marker.top}] sm:top-[${marker.topSm}]`
+            : `top-[${marker.top}]`;
 
-        return (
-          <div
-            key={marker.id}
-            className="group"
-            {...(marker.hasAnimation && { "data-aos": "zoom-in" })}
-          >
+          return (
             <div
-              className={`w-[${marker.width}] absolute ${topClass} ${leftClass} flex items-center justify-center cursor-pointer`}
+              key={marker.id}
+              className="group"
+              {...(marker.hasAnimation && { "data-aos": "zoom-in" })}
             >
-              <FaMapMarkerAlt
-                size={40}
-                className="text-red-600 group-hover:scale-125 transition-transform duration-200"
-              />
-              <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 absolute left-full top-1/2 -translate-y-1/2 ml-2 bg-white text-black text-xs rounded shadow-lg px-2 py-1 whitespace-nowrap z-10">
-                {marker.text}
-              </span>
+              <div
+                className={`w-[${marker.width}] absolute ${topClass} ${leftClass} flex items-center justify-center cursor-pointer`}
+              >
+                <FaMapMarkerAlt
+                  size={40}
+                  className="text-red-600 group-hover:scale-125 transition-transform duration-200"
+                />
+                <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 absolute left-full top-1/2 -translate-y-1/2 ml-2 bg-white text-black text-xs rounded shadow-lg px-2 py-1 whitespace-nowrap z-10">
+                  {marker.text}
+                </span>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
       {/* Map image at the bottom so markers are above */}
       <div className="w-full h-full">
         <Image
@@ -64,6 +67,7 @@ export default function Map({ markers }: MapProps) {
           width={1024}
           alt=""
           className="w-full h-auto"
+          onLoad={() => setImageLoaded(true)}
         />
       </div>
     </div>
