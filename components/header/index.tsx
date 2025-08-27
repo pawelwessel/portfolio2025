0e2df392213afaf374d440fd88baf2bd145c8ac1
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-08-27 20:31:45 +0200

    test

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,39 +1,9 @@
 "use client";
-import {
-  FaDownload,
-  FaHome,
-  FaPeopleArrows,
-  FaRegQuestionCircle,
-  FaUser,
-} from "react-icons/fa";
-import Link from "next/link";
 import { useEffect, useState } from "react";
-import { usePhoneModal } from "@/common/context/PhoneModalContext";
-import Faq from "../faq";
 import Cta from "../cta/Cta";
 export default function Header() {
-  const { open } = usePhoneModal();
-  const [isFaqOpen, setFaqOpen] = useState<boolean>(false);
-  const faqs = [
-    {
-      question: "Jak zamówić stronę internetową?",
-      answer:
-        "Wystarczy wypełnić krótki brief na stronie lub skontaktować się z nami mailowo. Odezwiemy się, by omówić szczegóły i przygotować ofertę.",
-    },
-    {
-      question: "Czy muszę znać się na stronach, by zamówić swoją?",
-      answer:
-        "Nie! Wszystko tłumaczymy prostym językiem i prowadzimy przez cały proces. Wystarczy, że opiszesz, czego potrzebujesz.",
-    },
-    {
-      question: "Ile kosztuje strona internetowa?",
-      answer:
-        "Każda wycena jest indywidualna i zależy od zakresu projektu. Po wypełnieniu briefu przygotujemy dla Ciebie jasną ofertę.",
-    },
-  ];
   const [supportsPWA, setSupportsPWA] = useState<any>(false);
   const [promptInstall, setPromptInstall] = useState<any>(null);
-
   useEffect(() => {
     const handler = (e: any) => {
       e.preventDefault();
