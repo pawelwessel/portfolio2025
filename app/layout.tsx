85df72db452e42d78f51570293de5d29772f506a
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-08-29 11:52:51 +0200

    test2

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,27 @@ export default async function Root({
             <PromoPopup />
           </PhoneModalProvider>
         </ThemeProvider>
+        <Script id="linkedin-analytics">
+          {`
+          
+_linkedin_partner_id = "8695857";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+(function(l) {
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+window.lintrk.q=[]}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);})(window.lintrk);
+
+<noscript>
+<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=8695857&fmt=gif" />
+</noscript>
+          `}
+        </Script>
         <Script
           async
           id="google-analytics"
