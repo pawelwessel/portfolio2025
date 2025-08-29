5a8748310d3f4bfe243194f46cd974fe11a3937e
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-08-30 00:33:30 +0200

    test

--- common/firebase/index.js
+++ common/firebase/index.js
@@ -14,13 +14,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY1,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN1,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID1,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET1,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID1,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID1,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURMENT_ID1,
 };
 const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
