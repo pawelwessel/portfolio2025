4d87813be67db8559a1924511beaae030bcbd88b
pawelwessel <wesiudev.rekrutacja@gmail.com>
2025-09-01 23:18:29 +0200

    Added some stuff to the landing page

--- personal-portfolio-v2-main/.history/i18n-config_20250331093217.ts
+++
@@ -1,5 +0,0 @@
-export const i18n = {
-  defaultLocale: "pl",
-  locales: ["en"],
-} as const;
-export type Locale = (typeof i18n)["locales"][number];
--- personal-portfolio-v2-main/app/[lang]/(admin)/admin/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-export default function Page() {
-  return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 p-6 sm:p-12 gap-3 sm:gap-6 text-white"></div>
-  );
-}
